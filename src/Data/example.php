8c3398a4d772332943f924b7411a876f4237907e
Visal <123046820+webdemo55@users.noreply.github.com>
2023-10-21 15:37:47 +0700

    [update]

--- src/Data/example.php
+++ src/Data/example.php
@@ -1,34 +1,48 @@
 <?php
+// filename: appsflyer_integration.php
+
 require 'vendor/autoload.php';
+
 use GuzzleHttp\Client;
 
-// Define constants for your API credentials
-define('APPSFLYER_API_URL', 'https://hq.appsflyer.com/export/');
-define('APP_ID', 'YOUR_APP_ID');  // Replace with your actual App ID
-define('API_TOKEN', 'YOUR_API_TOKEN'); // Replace with your actual API token
+class AppsFlyerIntegration {
+    const APPSFLYER_API_URL = 'https://hq.appsflyer.com/export/';
+    private $appId;
+    private $apiToken;
+    private $client;
 
-// Initialize the HTTP client
-$client = new Client();
+    public function __construct($appId, $apiToken) {
+        $this->appId = $appId;
+        $this->apiToken = $apiToken;
+        $this->client = new Client();
+    }
 
-// Make a request to the AppsFlyer API
-try {
-    $response = $client->get(APPSFLYER_API_URL . APP_ID . '/daily', [
-        'headers' => [
-            'Authorization' => 'Bearer ' . API_TOKEN
-        ],
-        'query' => [
-            // Add your query parameters here
-            'from' => 'yyyy-mm-dd',
-            'to' => 'yyyy-mm-dd'
-        ]
-    ]);
-
-    // Parse the response
-    $data = $response->getBody();
-    echo $data;
-
-} catch (Exception $e) {
-    echo "An error occurred: " . $e->getMessage();
+    public function fetchData($from, $to) {
+        try {
+            $response = $this->client->get(self::APPSFLYER_API_URL . $this->appId . '/daily', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->apiToken
+                ],
+                'query' => [
+                    'from' => $from,
+                    'to' => $to
+                ]
+            ]);
+            return json_decode($response->getBody(), true);
+        } catch (Exception $e) {
+            return "An error occurred: " . $e->getMessage();
+        }
+    }
 }
+
+$appId = 'YOUR_APP_ID'; // replace with your actual App ID
+$apiToken = 'YOUR_API_TOKEN'; // replace with your actual API token
+$fromDate = 'yyyy-mm-dd'; // replace with your start date
+$toDate = 'yyyy-mm-dd'; // replace with your end date
+
+$af = new AppsFlyerIntegration($appId, $apiToken);
+$data = $af->fetchData($fromDate, $toDate);
+print_r($data);
+
 
 ?>
